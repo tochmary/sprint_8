7d1d182f2541036a1755c62c36ce08306f7d14e7
tochmary <maria.tochka@yandex.ru>
2025-06-19 18:48:21 +0300

    для ошибки

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
@@ -63,6 +64,16 @@ public class Mesto1Test {
                 .auth().oauth2(bearerToken) // Передаём токен для аутентификации
                 .delete("/api/cards/{photoId}/likes", photoId) // Делаем DELETE-запрос
                 .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
 }
